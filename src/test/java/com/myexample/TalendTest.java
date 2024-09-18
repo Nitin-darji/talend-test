6f91d80306c9496cc248712e76fd2dcaa3c59b1e
Umang <umang@avanzar.in>
2024-09-18 10:54:32 +0530

    Initial commit for Talend test project

---
+++ src/test/java/com/myexample/TalendTest.java
@@ -0,0 +1,14 @@
+package com.myexample;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TalendTest {
+
+    @Test
+    public void testTalendIntegration() {
+        // Simulate a simple test case
+        System.out.println("Testing Talend Integration...");
+        assertTrue(true);
+    }
+}
